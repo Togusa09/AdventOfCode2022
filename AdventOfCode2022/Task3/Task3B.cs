930d9d2421fddc79c93d1e32b83788b20edfbb53
Ben Hockley <ben.hockley@purple.telstra.com>
2022-12-04 09:53:09 +1000

    Updated task 2 to use chunk

--- AdventOfCode2022/Task3/Task3B.cs
+++ AdventOfCode2022/Task3/Task3B.cs
@@ -10,13 +10,20 @@ public class Task3B
 
     public int Solve(IEnumerable<string> data)
     {
-        var dataArray = data.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
-        var result = 0;
-        for (var i = 0; i < dataArray.Length; i += 3)
-        {
-            var common = dataArray[i].Intersect(dataArray[i + 1]).Intersect(dataArray[i + 2]);
-            result += GetPriority(common.Single());
-        }
+        //var dataArray = data.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+        //var result = 0;
+        //for (var i = 0; i < dataArray.Length; i += 3)
+        //{
+        //    var common = dataArray[i].Intersect(dataArray[i + 1]).Intersect(dataArray[i + 2]);
+        //    result += GetPriority(common.Single());
+        //}
+
+        var result = data.Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(x => x.AsEnumerable())
+            .Chunk(3)
+            .Select(x => x.Aggregate((a, b) => a.Intersect(b)))
+            .Sum(x => GetPriority(x.Single()));
+
 
         return result;
     }
